3e0c8074800d0142aa8a4c9b536c433509933d78
Kevin Lavenant <kevinlavenant@MBP-de-Kevin.lan>
2023-12-02 14:02:25 +0100

    page d'accueil fonctionnelle (sauf boutons)

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -5,19 +5,12 @@ console.log(cursor);
 // ! Event
 // * animate div's cursor to follow the mouse
 document.addEventListener("mousemove", (event) => { // to have event when the mouse moves
-    cursor.setAttribute("style", `top:${event.pageY - 50}px; left:${event.pageX - 50}px;`);
+    cursor.setAttribute("style", `top:${event.pageY - 60}px; left:${event.pageX - 60}px;`);
     // to modify the position in the style with setAttribute
     // pageY and pageX = position of the mouse
     //  so .cursor get the top and left mouse's position
-    // NB : -40 is to divise the div's 80px by 2 to place the cursor on the middle
+    // NB : -60 is to divise the div's 120px by 2 to place the cursor on the middle
 });
 
-// ? animate document to get the cursor's scale animation on click
-// document.addEventListener("click", () => {
-//     cursor.classList.add("expand"); // to add the CSS class
 
-//     setTimeout(() => { // to remove the class after a defined time
-//         cursor.classList.remove("expand");
-//     }, 500); // the defined time is 500ms
-// });
 
