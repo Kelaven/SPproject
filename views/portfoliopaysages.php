80c8c35bbf8cffe1b6c88263580cd837b6bf5364
Kevin Lavenant <kevinlavenant@MBP-de-Kevin.lan>
2023-12-06 23:02:41 +0100

    partie paysages commencée

---
+++ views/portfoliopaysages.php
@@ -0,0 +1,17 @@
+<section>
+    <div class="parent">
+        <div class="div1"> <!-- parent of the second grid -->
+            <div class="divchild1"></div>
+            <div class="divchild2"></div>
+            <div class="divchild3"></div>
+            <div class="divchild4"></div>
+            <div class="divchild5"></div>
+            <div class="divchild6"></div>
+        </div>
+        <div class="div2"> </div>
+        <div class="div3"> </div>
+        <div class="div4"> </div>
+        <div class="div5"> </div>
+        <div class="div6"> </div>
+    </div>
+</section>
