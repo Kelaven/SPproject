f943e5860f7a573fd749d5022909c678215452d2
Kevin Lavenant <kevinlavenant@MacBook-Pro-de-Kevin.local>
2023-12-14 13:28:05 +0100

    forms complétés

--- views/accesclientform.php
+++ views/accesclientform.php
@@ -3,7 +3,7 @@
         <div class="accesclientform__overlay">
             <div class="col col-md-8 col-xl-5 p-0">
                 <h1 class="accesclientform__h1 pb-5">"Prénom" - séance portraits</h1> <!-- ! il faudra charger dynamiquement le prénom pour qu'il corresponde -->
-                <form action="" method="post" novalidate>
+                <form action="" method="post">
                     <!-- ! loginAccess -->
                     <div class="input-group pe-xl-3 ps-xl-3 px-1">
                         <label for="loginAccess"></label>
