c1786bfcb643adf0765d795e23c440a12b9cb785
Kevin Lavenant <kevinlavenant@MacBook-Pro-de-Kevin.local>
2023-12-14 13:17:44 +0100

    forms complétés

--- controllers/accesclientform-ctrl.php
+++ controllers/accesclientform-ctrl.php
@@ -1,20 +1,38 @@
 <?php
 
+// to have constants
+require_once __DIR__.'/../config/init.php';
 
 
 
+// ! cleaning and validation
+if ($_SERVER["REQUEST_METHOD"] == "POST") { // get the form's response
+    $error = [];
+    // ! loginAccess
+    $loginAccess = filter_input(INPUT_POST, 'loginAccess', FILTER_SANITIZE_SPECIAL_CHARS); // cleaning
+    if (empty($loginAccess)) { // to be sure it's not empty
+        $error['loginAccess'] = 'L\'identifiant n\'est pas renseigné';
+    } else { // validation
+        $isloginAccessOk = filter_var($loginAccess, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/'.REGEX_IDENTIFIANT.'/')));
+        if (!$isloginAccessOk) { // if it's not validate with the regex
+            $error['loginAccess'] = 'L\'identifiant n\'est pas correct';
+        }
+    }
 
+    // ? passwordAccess
+    $passwordAccess = filter_input(INPUT_POST, 'passwordAccess');
 
-
-
-
-
-
-
-
-
-
-
+    if (empty($passwordAccess)){
+        $error['passwordAccess'] = 'Le mot de passe n\'est pas renseigné';
+    } else {
+        $isPasswordAccessOk = filter_var($passwordAccess, FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => '/'.REGEX_PASSWORD.'/')));
+        if (!$isPasswordAccessOk) {
+            $error["passwordAccess"] = 'Le mots de passe est incorrect';
+        } else {
+            $passwordAccessHash = password_hash($passwordAccess, PASSWORD_DEFAULT);
+        }
+    }
+}
 
 
 
--- views/accesclientform.php
+++ views/accesclientform.php
@@ -4,20 +4,33 @@
             <div class="col col-md-8 col-xl-5 p-0">
                 <h1 class="accesclientform__h1 pb-5">"Prénom" - séance portraits</h1> <!-- ! il faudra charger dynamiquement le prénom pour qu'il corresponde -->
                 <form action="" method="post" novalidate>
-                    <!-- ! Login -->
+                    <!-- ! loginAccess -->
                     <div class="input-group pe-xl-3 ps-xl-3 px-1">
-                        <label for="login"></label>
-                        <span class="input-group-text" id="span__login"><i class="fa-solid fa-user"></i></span>
-                        <input required type="text" name="login" id="login" placeholder="Identifiant" class="form-control" value="<?= $login ?? '' ?>">
+                        <label for="loginAccess"></label>
+                        <span class="input-group-text"><i class="fa-solid fa-user"></i></span>
+                        <input 
+                        required 
+                        type="text" 
+                        name="loginAccess" 
+                        id="loginAccess" 
+                        placeholder="Identifiant" 
+                        class="form-control" 
+                        value="<?= $loginAccess ?? '' ?>">
                     </div>
-                    <small class="text-danger ps-3"><?= $error['login'] ?? '' ?></small>
-                    <!-- ! Password -->
+                    <small class="text-danger ps-3"><?= $error['loginAccess'] ?? '' ?></small>
+                    <!-- ! passwordAccess -->
                     <div class="input-group pe-xl-3 ps-xl-3 px-1">
-                        <label for="password"></label>
-                        <span class="input-group-text" id="span__password"><i class="fa-solid fa-lock"></i></span>
-                        <input required type="password" name="password" id="password" placeholder="Mot de passe" class="form-control">
+                        <label for="passwordAccess"></label>
+                        <span class="input-group-text"><i class="fa-solid fa-lock"></i></span>
+                        <input 
+                        required 
+                        type="password" 
+                        name="passwordAccess" 
+                        id="passwordAccess" 
+                        placeholder="Mot de passe" 
+                        class="form-control">
                     </div>
-                    <small class="text-danger ps-3"><?= $error['password'] ?? '' ?></small>
+                    <small class="text-danger ps-3"><?= $error['passwordAccess'] ?? '' ?></small>
                     <!-- ! validation btn -->
                     <div class="text-center py-3">
                         <button class="btn btn-primary" type="submit">Envoyer</button>
