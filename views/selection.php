34c9168eaedf98721a459f5f47cde6a1e384262e
Kelaven <kevin.lavenant@live.fr>
2024-01-16 19:35:37 +0100

    animation sélection OK

--- views/selection.php
+++ views/selection.php
@@ -1,11 +1,17 @@
 <section class="selection__section">
     <div class="container">
         <div class="row">
-            <div class="col selection__col"></div>
-            <div class="col selection__col selection__col--img d-flex align-content-center">
+            <div class="col-4 selection__col"></div>
+            <!-- <div class="col-4 selection__col selection__col--img d-flex align-content-center">
                 <img class="selection__img" src="/public/assets/img/jpegpaysages/modalsize/selection-grenoble-vercors-mountains-1440-100.jpg" alt="Image de lumière traversant les nuages dans la montagne en France">
+            </div> -->
+            <div class="col-4 selection__col d-flex align-items-center">
+                <div class="screen">
+                    <div class="screen__img"></div> <!-- ???? insérer une aria description pour faire office de alt sachant que l'img sera utilisée en background-image (et la rendre dynamique) ???? -->
+                    <!-- !!!! NB : il sera possible de changer dynamiquement le background-image avec php en se servant des balises script dans le html pr fr des variables, voir exo POO -->
+                </div>
             </div>
-            <div class="col selection__col "></div>
+            <div class="col-4 selection__col "></div>
         </div>
     </div>
 </section>
