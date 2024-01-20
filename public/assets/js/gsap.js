92c0d6115e70c72085ae263d455b21d40a6e51ed
Kelaven <kevin.lavenant@live.fr>
2024-01-20 18:30:13 +0100

    animation ok

--- public/assets/js/gsap.js
+++ public/assets/js/gsap.js
@@ -53,24 +53,24 @@ function loader() {
 
 
 
-// // * animate selection text
+// * animate selection text
 
-// gsap.registerPlugin(TextPlugin);
+gsap.registerPlugin(TextPlugin);
 
-// const h1toAnimate = document.querySelector('.h1__scramble');
+const h1toAnimate = document.querySelector('.h1__scramble');
 
-// const scrambleAnimation = gsap.timeline({ paused: true }); // to store GSAP animation but don't play now
+const scrambleAnimation = gsap.timeline({ paused: true }); // to store GSAP animation but don't play now
 
-// scrambleAnimation.to(h1toAnimate, { // the animation is to the h1 selected
-//     text: { value: "Chaque image raconte une histoire, laissons-nous emporter.", scrambleText: true },
-//     duration: 1.5,
-//     ease: "power1.inOut"
-// });
+scrambleAnimation.to(h1toAnimate, { // the animation is to the h1 selected
+    text: { value: "Chaque image raconte une histoire, laissons-nous emporter.", scrambleText: true },
+    duration: 1.5,
+    ease: "power1.inOut"
+});
 
-// h1toAnimate.addEventListener('mouseenter', () => {
-//     scrambleAnimation.play(); // launch animation
-// });
+h1toAnimate.addEventListener('mouseenter', () => {
+    scrambleAnimation.play(); // launch animation
+});
 
-// h1toAnimate.addEventListener('mouseleave', () => {
-//     scrambleAnimation.reverse(); // reverse animation
-// });
+h1toAnimate.addEventListener('mouseleave', () => {
+    scrambleAnimation.reverse(); // reverse animation
+});
